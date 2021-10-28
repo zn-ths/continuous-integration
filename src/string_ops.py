ba2da52fc05f19fa212ea43fb50d2f87a961a15b
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-10-28 21:00:38 +0200

    add docstrings to remove_vowels and reverse_string functions

--- src/string_ops.py
+++ src/string_ops.py
@@ -2,10 +2,26 @@ VOWELS = ["a", "e", "i", "o", "u", "y"]
 
 
 def remove_vowels(word: str) -> str:
+    """Returns a string without any vowels: aeiouy.
+
+    Args:
+        word (str): the string to remove vowels from.
+    Returns:
+        str: string without any vowels.
+    """
+
     return "".join([letter for letter in word if letter.lower() not in VOWELS])
 
 
 def reverse_string(word: str) -> str:
+    """Returns a reversed string.
+
+    Args:
+        word (str): the string to reverse.
+    Returns:
+        str: the reversed string.
+    """
+
     return word[::-1]
 
 
